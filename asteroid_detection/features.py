bba712d86b137eda11f1b25619fe0c5764d7467b
cmccachern <carey@mccachern.com>
2019-11-03 13:15:10 -0700

    Add request_cache to sdss to cache previously fetched data.

--- asteroid_detection/features.py
+++ asteroid_detection/features.py
@@ -3,7 +3,7 @@ Functions for manipulating fits images to find asteroids.
 """
 
 #pylint: disable=no-member
-#pylint: disable=unsubscriptable-object 
+#pylint: disable=unsubscriptable-object
 
 
 import numpy as np
