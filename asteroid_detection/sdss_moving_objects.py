ddb209547ef224964932d5d5b2a00199a9f5512e
cmccachern <carey@mccachern.com>
2019-11-16 10:19:18 -0700

    Make a function to get asteroid coordinates for a rcf

--- asteroid_detection/sdss_moving_objects.py
+++ asteroid_detection/sdss_moving_objects.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from features import fits_from_rcf, align_images, jpg_resized, display_coordinates, find_asteroids
+from features import get_aligned_images, jpg_resized, display_coordinates, find_asteroids
 
 def get_moving_objects(filename = "~/Downloads/ADR4.dat"):
     df = pd.read_csv(filename, delim_whitespace=True)
@@ -16,24 +16,22 @@ def asteroids_in_rcf(df, run, camcol, field):
      
     return df
 
-
-if __name__ == "__main__":
-    run, camcol, field = 752, 1, 321
+def asteroid_coords_in_rcf(run, camcol, field):
     df = get_moving_objects()
-    asteroids = asteroids_in_rcf(df, 752,1,321)
-    fits_file = fits_from_rcf(run, camcol, field)
-    images = align_images(fits_file) 
-    jpg_img = jpg_resized(run, camcol, field, images["r"].shape)
-    #save_coordinates(jpg_img, asteroids, "asteroids", run, camcol, field)
+    asteroids = asteroids_in_rcf(df, run, camcol, field)
     asteroid_coordinates = np.array(list((zip(asteroids["rowc"], asteroids["colc"]))))
     asteroid_coordinates = np.round(asteroid_coordinates)
     asteroid_coordinates = asteroid_coordinates.astype(int)
-    print(asteroid_coordinates)
+    return asteroid_coordinates
 
-
+if __name__ == "__main__":
+    run, camcol, field = 752, 1, 321
+    images = get_aligned_images(run, camcol, field)
+    asteroid_coordinates = asteroid_coords_in_rcf(run, camcol, field)
     asteroids = find_asteroids(images, asteroid_coordinates)
     print(asteroids)
 
+    jpg_img = jpg_resized(run, camcol, field, images["r"].shape)
     display_coordinates(jpg_img, asteroid_coordinates)
 
 
